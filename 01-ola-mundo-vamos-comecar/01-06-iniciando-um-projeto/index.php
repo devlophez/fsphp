7860db44dda8d1591883e3232d291998922f47ae
pedroleandro <pedro.leandrog@gmail.com>
2020-06-16 23:55:53 -0300

    first commit

---
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FSPHP do Jeito Certo</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<?php
+    $start = "Vamos começar!";
+    echo "<h1>Olá, Mundo! {$start}</h1>";
+    echo "<p id='js'>Loading...</p>"
+?>
+
+<script src="assets/js/script.js"></script>
+</body>
+</html>
