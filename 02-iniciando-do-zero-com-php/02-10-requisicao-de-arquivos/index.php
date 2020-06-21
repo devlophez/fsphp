1451b464650b34025ef0d10e4c9a55eff1f67ae0
pedroleandro <pedro.leandrog@gmail.com>
2020-06-20 23:54:28 -0300

    requisição de arquivos

--- 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
@@ -8,6 +8,28 @@ fullStackPHPClassName("02.10 - Requisição de arquivos");
  */
 fullStackPHPClassSession("include, include_once", __LINE__);
 
+//include "file.php";
+//echo "<p>Continue</p>";
+
+//include "header.php";
+include __DIR__ . "/header.php";
+
+/**
+ * StdClass representa um objeto dinâmico ou um vetor de dados
+ */
+
+$profile = new StdClass();
+$profile->name = "Pedro";
+$profile->company = "CredVip";
+$profile->email = "pedro.leandrogs@gmail.com";
+include __DIR__ . "/profile.php";
+
+$profile->name = "Airton";
+$profile->company = "CredVip";
+$profile->email = "airton.sousas@gmail.com";
+include __DIR__ . "/profile.php";
+
+//var_dump($profile);
 
 /*
  * [ require ] https://php.net/manual/pt_BR/function.require.php
@@ -15,3 +37,8 @@ fullStackPHPClassSession("include, include_once", __LINE__);
  */
 fullStackPHPClassSession("require, require_once", __LINE__);
 
+//require "file.php";
+//echo "<p>Continue</p>";
+
+require __DIR__ . "/config.php";
+echo "<h1>" . COURSE . "</h1>";
---
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/profile.php
@@ -0,0 +1,12 @@
+<article style="
+    padding: 5px 20px;
+    background: #eeeeee;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+">
+    <h1><?= $profile->name; ?></h1>
+    <p>Trabalha na <?= $profile->company; ?><br>
+        <a title="Enviar e-mail" href="mailto: <?= $profile->email; ?>">Enviar e-mail</a>
+    </p>
+</article>
