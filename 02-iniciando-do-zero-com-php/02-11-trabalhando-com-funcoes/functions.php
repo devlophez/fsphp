e2ad6b8821b970257aa0bc69974c9b6848c43b4a
pedroleandro <pedro.leandrog@gmail.com>
2020-06-21 00:24:22 -0300

    trabalhando com funções

---
+++ 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+function functionName($arg1, $arg2, $arg3)
+{
+    $body = [$arg1, $arg2, $arg3];
+    return $body;
+}
+
+/**
+ * Sequência correta de parâmetros de funções:
+ * obrigatórios,
+ * importantes (já vem preenchidos com o valor padrão)
+ * e opcionais (recebem null) não precisam necessariamente serem preenchidos
+ */
+
+function optionArgs($arg1, $arg2 = true, $arg3 = null)
+{
+    $body = [$arg1, $arg2, $arg3];
+    return $body;
+}
+
+function calcImc()
+{
+    global $weight;
+    global $height;
+    return $weight / ($height * $height);
+}
+
+function payTotal($price)
+{
+    static $total;
+    $total += $price;
+    return "<p>O total a pagar é R$ ".number_format($total, "2", ",", ".")."</p>";
+}
+
+function myTeam(){
+    $teamNames = func_get_args();
+    $teamCount = func_num_args();
+    return ["members" => $teamNames, "count" => $teamCount];
+}
--- 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
+++ 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
@@ -7,20 +7,36 @@ fullStackPHPClassName("02.11 - Trabalhando com funções");
  */
 fullStackPHPClassSession("functions", __LINE__);
 
+require __DIR__ . "/functions.php";
+var_dump(functionName("Atendimento", "Suporte", "Desenvolvimento"));
+var_dump(functionName("Leneia", "Igo", "Pedro"));
+
+var_dump(optionArgs("Pedro"));
+var_dump(optionArgs("Pedro", "Airton"));
+var_dump(optionArgs("Pedro", "Airton", "Anderson"));
 
 /*
  * [ global access ] global $var
  */
 fullStackPHPClassSession("global access", __LINE__);
 
+$weight = 62;
+$height = 1.68;
+echo calcImc();
 
 /*
  * [ static arguments ] static $var
  */
 fullStackPHPClassSession("static arguments", __LINE__);
 
+$pay = payTotal(200);
+$pay = payTotal(150);
+$pay = payTotal(300);
+echo $pay;
 
 /*
  * [ dinamic arguments ] get_args | num_args
  */
 fullStackPHPClassSession("dinamic arguments", __LINE__);
+
+var_dump(myTeam("Pedro", "Airton", "Juan", "Gerson"));
