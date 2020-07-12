eebc661517935b5e3dc2b108aab1a13a3039ea0b
pedroleandro <pedro.leandrog@gmail.com>
2020-07-12 13:33:05 -0300

    interpretação e operações - parte 1

--- 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/index.php
+++ 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/index.php
@@ -10,6 +10,15 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("__construct", __LINE__);
 
+//$user = new \Source\Interpretation\User();
+
+$user = new \Source\Interpretation\User(
+    "Pedro Leandro",
+    "Gomes da Silva",
+    "ti@credvip.com"
+);
+
+var_dump($user);
 
 /*
  * [ clone ] Executado automaticamente quando um novo objeto é criado a partir do operador clone.
@@ -17,6 +26,26 @@ fullStackPHPClassSession("__construct", __LINE__);
  */
 fullStackPHPClassSession("__clone", __LINE__);
 
+$pedro = $user;
+$airton = $pedro;
+
+$airton->setFirstName("Airton");
+$airton->setLastName("Sousa");
+
+$pedro->setFirstName("Pedro Leandro");
+$pedro->setLastName("Gomes da Silva");
+
+$airton = clone $pedro;
+$airton->setFirstName("Airton");
+$airton->setLastName("Sousa");
+
+$gerson = clone $pedro;
+
+var_dump(
+    $pedro,
+    $airton,
+    $gerson
+);
 
 /*
  * [ destruct ] Executado automaticamente quando o objeto é finalizado
