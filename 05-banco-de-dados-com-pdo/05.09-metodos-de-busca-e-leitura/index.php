a6f3ea541c4442a48e58a7e046f5e64bd86216db
pedroleandro <pedro.leandrog@gmail.com>
2020-07-31 21:13:38 -0300

    métodos de busca e leitura

--- 05-banco-de-dados-com-pdo/05.09-metodos-de-busca-e-leitura/index.php
+++ 05-banco-de-dados-com-pdo/05.09-metodos-de-busca-e-leitura/index.php
@@ -9,15 +9,30 @@ require __DIR__ . "/../source/autoload.php";
  */
 fullStackPHPClassSession("load", __LINE__);
 
+$model = new \Source\Models\UserModel();
+
+$user = $model->load(1);
+var_dump($user, "{$user->first_name} {$user->last_name}");
 
 /*
  * [ find ] Por indexes da tabela (email)
  */
 fullStackPHPClassSession("find", __LINE__);
 
+$user = $model->find("mateus31@email.com.br");
+var_dump($user, "{$user->first_name} {$user->last_name}");
 
 /*
  * [ all ] Retorna diversos registros
  */
 fullStackPHPClassSession("all", __LINE__);
 
+$all = $model->all(5);
+//var_dump($all);
+
+/**
+ * @var \Source\Models\UserModel $user
+ */
+foreach ($all as $user){
+    var_dump("{$user->first_name} {$user->last_name}");
+}
--- 05-banco-de-dados-com-pdo/source/Models/Model.php
+++ 05-banco-de-dados-com-pdo/source/Models/Model.php
@@ -4,6 +4,8 @@
 namespace Source\Models;
 
 
+use Source\Database\Connect;
+
 abstract class Model
 {
     /**
@@ -22,6 +24,38 @@ abstract class Model
     protected $message;
 
     /**
+     * quando for atribuido uma propriedade que não esteja acessível
+     * será manipulada e enviada para a camada de data
+     */
+    public function __set($name, $value)
+    {
+        if(empty($this->data)){
+            $this->data = new \stdClass();
+        }
+
+        $this->data->$name = $value;
+    }
+
+    /**
+     * - Vai servir para validar se aquele recurso realmente é válido
+     */
+
+    public function __isset($name)
+    {
+        return isset($this->data->$name);
+    }
+
+    /**
+     * - Vai servir para obter os recursos.
+     * - Será executada toda vez que for tentado acessar
+     * uma propriedade que não existe ou está inacessível.
+     */
+    public function __get($name)
+    {
+        return ($this->data->$name ?? null);
+    }
+
+    /**
      * @return object|null
      */
     public function data(): ?object
@@ -32,7 +66,7 @@ abstract class Model
     /**
      * @return \PDOException
      */
-    public function fail(): \PDOException
+    public function fail(): ?\PDOException
     {
         return $this->fail;
     }
@@ -50,9 +84,26 @@ abstract class Model
 
     }
 
-    protected function read()
+    protected function read(string $select, $params = null): ?\PDOStatement
     {
+        try {
+            $stmt = Connect::getInstance()->prepare($select);
 
+            if ($params) {
+                parse_str($params, $params);
+                foreach ($params as $key => $value) {
+                    $type = (is_numeric($value) ? \PDO::PARAM_INT : \PDO::PARAM_STR);
+                    $stmt->bindValue(":{$key}", $value, $type);
+                }
+            }
+
+            $stmt->execute();
+            return $stmt;
+
+        } catch (\PDOException $exception) {
+            $this->fail = $exception;
+            return null;
+        }
     }
 
     protected function update()
