d7298c75906e612ebcb21bb54293d81908bb33ec
pedroleandro <pedro.leandrog@gmail.com>
2020-08-21 02:01:24 -0300

    acesso e controle de sessões

--- 06-seguranca-e-boas-praticas/06-04-acesso-e-controle-de-sessoes/index.php
+++ 06-seguranca-e-boas-praticas/06-04-acesso-e-controle-de-sessoes/index.php
@@ -8,3 +8,26 @@ require __DIR__ . "/../source/autoload.php";
  * [ session ] Uma classe statless para manipulação de sessões
  */
 fullStackPHPClassSession("session", __LINE__);
+
+$session = new \Source\Core\Session();
+//$session->set("user", 1);
+//$session->regenerate();
+//
+//$session->set("stats", ["name", "email"]);
+//$session->unset("stats");
+//
+//if (!$session->has("login")) {
+//    echo "<p>Logar-se</p>";
+//    $user = (new \Source\Models\User())->load(1);
+//    $session->set("login", $user);
+//}
+
+$session->destroy();
+
+
+var_dump(
+    $_SESSION,
+    $session->all()
+//    $session->user,
+//    session_id()
+);
--- 06-seguranca-e-boas-praticas/source/Support/Config.php
+++ 06-seguranca-e-boas-praticas/source/Support/Config.php
@@ -12,12 +12,22 @@ define("CONF_DB_NAME", "fsphp_pdo");
 /**
  * PROJECT URLs
  */
+
+//Windows
+//define("CONF_URL_BASE", "http://localhost/fsphp");
+
+//Linux
 define("CONF_URL_BASE", "http://localhost/web/upinside/fsphp");
 define("CONF_URL_ADMIN", CONF_URL_BASE . "/admin");
-define("CONF_URL_ERROR", CONF_URL_BASE."/404");
+define("CONF_URL_ERROR", CONF_URL_BASE . "/404");
 
 /**
  * DATES
  */
 define("CONF_DATE_BR", "d/m/Y H:i:s");
 define("CONF_DATE_APP", "Y-m-d H:i:s");
+
+/**
+ * SESSIONS
+ */
+define("CONF_SES_PATH", __DIR__ . "/../../storage/sessions");
